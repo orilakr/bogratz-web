ab44a23fb4f918d6441c6158de97dfd2a66f178a
Ori <orilakr@gamil.com>
2025-09-25 21:48:54 +0300

    with daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,6 +7,7 @@ function App() {
   return (
     <>
       <h1 class="text-red-500">Vite + React</h1>
+      <button className="btn btn-info">Info</button>
     </>
   )
 }
