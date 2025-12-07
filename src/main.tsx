fff63253be0757c8bf496a7c9f4c97a3edae9465
Ori <orilakr@gamil.com>
2025-12-07 18:00:00 +0200

    delete test file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import SimpleCounter from './test.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
